5c093de1dbfc28adb515b8dbee25f7e88abf1c6b
hiimmax99 <hiimmax99@gmail.com>
2021-03-17 10:14:41 +0700

    Bike

---
+++ addproduct.php
@@ -0,0 +1,36 @@
+<?php
+include 'config.php';
+if(isset($_POST['submit'])){
+	$TenXe = $_POST['TenXe'];
+	$BienSoXe = $_POST["BienSoXe"];
+    $KhungXe = $_POST["KhungXe"];
+    $MauSac = $_POST["MauSac"];
+	$GiaThanh = $_POST["GiaThanh"];
+    $NamDangKy = $_POST["NamDangKy"];
+    $TrangThai = $_POST["TrangThai"];
+    $TenLoaiXe = $_REQUEST["TenLoaiXe"];
+
+	//image upload
+
+	$msg = "";
+	$HinhAnh = $_FILES['HinhAnh']['name'];
+	$target = "img/".basename($HinhAnh);
+
+	if (move_uploaded_file($_FILES['HinhAnh']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+
+  	$insert_data = "INSERT INTO chitietxe (TenXe,BienSoXe,KhungXe,MauSac,GiaThanh,NamDangKy,TrangThai,TenLoaiXe) VALUES ('$TenXe','$BienSoXe','$KhungXe','$MauSac','$GiaThanh','$NamDangKy','$TrangThai','$TenLoaiXe')";
+  	 
+    $run_data = mysqli_query($link,$insert_data);
+
+  	if($run_data){
+  		header('location:product.php');
+  	}else{
+  		echo "Thêm xe không thành công";
+  	}
+
+}
+?>
---
+++ updateproduct.php
@@ -0,0 +1,38 @@
+<?php
+include 'config.php';
+$MaXe = $_GET['MaXe'];
+
+if(isset($_POST['submit']))
+{
+	$TenXe = $_POST["TenXe"];
+	$BienSoXe = $_POST["BienSoXe"];
+    $KhungXe = $_POST["KhungXe"];
+    $MauSac = $_POST["MauSac"];
+	$GiaThanh = $_POST["GiaThanh"];
+    $NamDangKy = $_POST["NamDangKy"];
+    $TrangThai = $_POST["TrangThai"];
+    // $TenLoaiXe = $_POST["TenLoaiXe"];        
+	$TenLoaiXe = $_REQUEST["TenLoaiXe"];
+ 
+
+	$msg = "";
+	$HinhAnh = $_FILES['HinhAnh']['name'];
+	$target = "img/".basename($HinhAnh);
+
+	if (move_uploaded_file($_FILES['HinhAnh']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+
+	$update = "UPDATE chitietxe SET TenXe='$TenXe', BienSoXe = '$BienSoXe', KhungXe = '$KhungXe', MauSac = '$MauSac', GiaThanh = '$GiaThanh', NamDangKy = '$NamDangKy', MaLoaiXe='$TenLoaiXe' TrangThai = '$TrangThai', HinhAnh = '$HinhAnh'  WHERE MaXe = $MaXe ";
+	$run_update = mysqli_query($link,$update);
+
+	if($run_update){
+		header('location:product.php');
+	}else{
+		echo "Dữ liệu không được cập nhật!";
+	}
+}
+
+?>
